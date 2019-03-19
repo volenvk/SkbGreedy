23113b179808cf973debd8e48a279bd733045bfd
ValeriyKov <valeriy.kov@hotmail.com>
2019-03-19 12:22:48 +0500

    First test completed

---
+++ DijkstraPathFinder.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greedy.Architecture;
+using System.Drawing;
+
+namespace Greedy
+{  
+    public class DijkstraPathFinder
+    {
+        public IEnumerable<PathWithCost> GetPathsByDijkstra(State state, Point start, IEnumerable<Point> targets)
+        {
+            if(targets == null || !targets.Any()) yield break;
+            var frontier = new Queue<Point>();
+            frontier.Enqueue(start);
+            var cameFrom = new Dictionary<Point, PathWithCost> {[start] = new PathWithCost(0, start)};
+            var costSoFar = new Dictionary<Point, int> {[start] = 0};
+
+            while (frontier.Any())
+            {
+                var current = frontier.Dequeue();
+
+                foreach (Point next in Nodes(current))
+                {
+                    if (!state.InsideMap(next)) continue;
+                    if (state.IsWallAt(next)) continue;
+                    var newCost = costSoFar[current] + state.CellCost[next.X, next.Y];
+                    if (costSoFar.ContainsKey(next) && newCost >= costSoFar[next]) continue;
+                    costSoFar[next] = newCost;
+                    frontier.Enqueue(next);
+                    var paths = new List<Point>(cameFrom[current].Path){next};
+                    cameFrom[next] = new PathWithCost(newCost, paths.ToArray());
+                }
+            }
+
+            foreach (var target in targets)
+            {
+                if (!cameFrom.ContainsKey(target)) continue;
+                yield return cameFrom[target];   
+            }
+        }
+        
+        private IEnumerable<Point> Nodes(Point point)
+        {
+            for (var dy = -1; dy <= 1; dy++)
+            for (var dx = -1; dx <= 1; dx++)
+                if (dx != 0 && dy != 0)
+                    continue;
+                else
+                {
+                    yield return new Point
+                    {
+                        X = point.X + dx,
+                        Y = point.Y + dy
+                    };
+                }
+        }
+    }
+}
---
+++ GreedyPathFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Greedy.Architecture;
+using Greedy.Architecture.Drawing;
+
+namespace Greedy
+{
+	public class GreedyPathFinder : IPathFinder
+	{
+		public List<Point> FindPathToCompleteGoal(State state)
+		{
+			return new List<Point>();
+		}
+	}
+}
