f38c71bcd8a3e6e6b761ee6cb326731c96fca428
ValeriyKov <valeriy.kov@hotmail.com>
2019-03-25 14:19:23 +0500

    it's right algorithm ToDo

--- GreedyPathFinder.cs
+++ GreedyPathFinder.cs
@@ -19,18 +19,21 @@ namespace Greedy
             if (state.Goal > points.Count) return new List<Point>();
             while (points.Count > 0)
             {
-                foreach (var edge in GetPaths(state, last, points).OrderBy(x=>x.Cost))
+                foreach (var edge in GetSortPaths(state, last, points))
                 {
                     energy -= edge.Cost;
-                    if (energy < 0) break;
                     tree.Add(edge);
                     if (HasCycle(tree))
+                    {
                         tree.Remove(edge);
+                        continue;
+                    }
                     
                     last = edge.End;
                     points.Remove(last);
                     break;
                 }
+                if (energy < 0) break;
             }
             
             var result = new List<Point>();
@@ -42,13 +45,24 @@ namespace Greedy
 
         private static bool HasCycle(List<PathWithCost> edges)
         {
-            return edges.All(x => edges.All(y => y.Path.SequenceEqual(x.Endpoints.Reverse())));
+            var e = edges.GetEnumerator();
+            if (!e.MoveNext()) return false;
+            var last = e.Current;
+            var result = false;
+            while (e.MoveNext())
+            {
+                var prev = e.Current;
+                var items = prev.Path.Except(prev.Path);
+                result = items.Any();
+            }
+
+            return result;
         }
 
-        private IEnumerable<PathWithCost> GetPaths(State state, Point start, IEnumerable<Point> cheats)
+        private IEnumerable<PathWithCost> GetSortPaths(State state, Point start, IEnumerable<Point> cheats)
         {
             return cheats.Select(cheat => finder.GetPathsByDijkstra(state, start, new []{cheat})
-                .First());
+                .First()).OrderBy(x=>x.Cost);
         }
     }
 }
